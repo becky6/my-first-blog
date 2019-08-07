bfe0ec7f1acd96ff928fd6cdc71bb4a84022cc8b
rebecca <beckyawal6@gmail.com>
2019-08-07 11:10:58 -0300

    My Django Girls app, first commit

---
+++ Becca.py
@@ -0,0 +1,35 @@
+#if 3>2:
+#print('it works')
+#if 5>2:
+#	print('5 is indeed greater than2')
+#else:
+ #   print('5 is not greater than 2')
+
+#if name == 'Ola':
+#	print('Hey Ola')
+#	print('Hey Sonja!')
+#	print('Hey anonymous!')
+#def hi():
+#	print('Hi there!')
+#	print('How are you?')
+
+#
+#	if name =='Ola':
+#		print('Hi Ola!')
+#	elif name == 'Sonja':
+#		print('Hi Sonja!')
+#	else:
+#		print('Hi anonymous!')
+#hi('Ola')	
+#	print('hi ' + name + '! it is nice to meet you. how are you doing? \n')
+#hi('Barbara')
+#
+
+#def hi(name):
+#	print('hi' + name + '!')
+
+#girls = ['Rachel', 'Monica', 'Phoebe', 'Ola']
+#	hi(name)
+#	print('next girl')	
+for i in range(1, 6):
+	print(i)
